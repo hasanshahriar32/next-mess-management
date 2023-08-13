b03b72f4e86fb03e9c8cee8868edabf34d45a687
itzshakeeb <itzshakeeb@gmail.com>
2023-08-13 22:33:12 +0600

    starting hero

---
+++ src/Components/Home/Hero.tsx
@@ -0,0 +1,13 @@
+import Container from "../ui/Container/container";
+
+const Hero = () => {
+  return (
+    <div>
+      <Container>
+        <>loading</>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
--- src/app/(with-layout)/page.tsx
+++ src/app/(with-layout)/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import Container from "@/Components/ui/Container/container";
-import { P, Subtitle, Title } from "@/Components/ui/Heading/Heading";
+import Hero from "@/Components/Home/Hero";
 
 export default function Home() {
   return (
     <>
-      <Container>
-        <Title className="min-h-[60vh]">this is title</Title>
-        <Subtitle>this sub title</Subtitle>
-        <P>this is Paramargah</P>
-      </Container>
+      <Hero />
     </>
   );
 }
