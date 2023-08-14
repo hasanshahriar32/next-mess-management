623317dd80bd718cd3194689bc25008291deb472
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-14 14:01:06 +0600

    update

--- src/Components/BazarDate/BazarDate.jsx
+++ src/Components/BazarDate/BazarDate.jsx
@@ -1,55 +1,73 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+// ... (previous imports and code)
 
-export default class BazarDate extends React.Component {
-  state = {
-    events: [],
+export default function BazarDate() {
+  const { data } = useSession();
+  const [events, setEvents] = useState([]);
+  const [tooltipEvent, setTooltipEvent] = useState(null);
+
+  const handleDeleteEvent = (eventToDelete) => {
+    if (window.confirm("Are you sure you want to delete this event?")) {
+      setEvents(events.filter((event) => event !== eventToDelete));
+    }
   };
 
-  render() {
-    return (
-      <div className="container mx-auto p-4">
-        <FullCalendar
-          plugins={[
-            dayGridPlugin,
-            interactionPlugin,
-            timeGridPlugin,
-            listPlugin,
-          ]}
-          themeSystem="bootstrap"
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "timeGridWeek,dayGridMonth,listMonth",
-          }}
-          initialView="dayGridMonth"
-          dateClick={this.handleDateClick}
-          weekends={false}
-          nowIndicator={true}
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          eventContent={this.renderEventContent}
-          events={this.state.events}
-        />
-      </div>
-    );
-  }
+  const handleDateClick = (arg) => {
+    if (
+      window.confirm("Would you like to add an event to " + arg.dateStr + " ?")
+    ) {
+      const title = prompt("Enter Event Title");
+      const description = prompt("Enter Event Description");
+      if (title && description) {
+        setEvents([
+          ...events,
+          {
+            title: title,
+            description: description,
+            start: arg.date,
+            allDay: true,
+          },
+        ]);
+      }
+    }
+  };
 
-  renderEventContent = (eventInfo) => {
+  const handleEventMouseEnter = (arg) => {
+    setTooltipEvent(arg.event);
+  };
+
+  const handleEventMouseLeave = () => {
+    setTooltipEvent(null);
+  };
+
+  const renderEventContent = (eventInfo) => {
     return (
-      <div className="flex justify-between items-center p-2 bg-gray-100 rounded">
+      <div
+        className="flex justify-between items-center p-2 bg-gray-100 rounded"
+        onMouseEnter={() => handleEventMouseEnter(eventInfo)}
+        onMouseLeave={handleEventMouseLeave}
+      >
         <p className="font-semibold text-gray-800">{eventInfo.timeText}</p>
+        {/* set image  */}
+        <Image
+          className="object-cover rounded-full h-7 w-7"
+          src={data?.user?.image || ""}
+          alt="avatar"
+          width={30}
+          height={30}
+        />
         <p className="text-gray-800">{eventInfo.event.title}</p>
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-sm"
-          onClick={() => this.handleDeleteEvent(eventInfo.event)}
+          onClick={() => handleDeleteEvent(eventInfo.event)}
         >
           Delete
         </button>
@@ -57,33 +75,34 @@ export default class BazarDate extends React.Component {
     );
   };
 
-  handleDeleteEvent = (eventToDelete) => {
-    if (window.confirm("Are you sure you want to delete this event?")) {
-      this.setState({
-        events: this.state.events.filter((event) => event !== eventToDelete),
-      });
-    }
-  };
-
-  handleDateClick = (arg) => {
-    if (
-      window.confirm("Would you like to add an event to " + arg.dateStr + " ?")
-    ) {
-      const title = prompt("Enter Event Title");
-      const description = prompt("Enter Event Description");
-      if (title && description) {
-        this.setState({
-          events: [
-            ...this.state.events,
-            {
-              title: title,
-              description: description,
-              start: arg.date,
-              allDay: true,
-            },
-          ],
-        });
-      }
-    }
-  };
+  return (
+    <div className="container mx-auto p-4">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+        themeSystem="bootstrap"
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "timeGridWeek,dayGridMonth,listMonth",
+        }}
+        initialView="dayGridMonth"
+        dateClick={handleDateClick}
+        eventMouseEnter={handleEventMouseEnter}
+        eventMouseLeave={handleEventMouseLeave}
+        weekends={false}
+        nowIndicator={true}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        eventContent={renderEventContent}
+        events={events}
+      />
+      {tooltipEvent && (
+        <div className="tooltip">
+          <p>{tooltipEvent.title}</p>
+          <p>{tooltipEvent.extendedProps.description}</p>
+        </div>
+      )}
+    </div>
+  );
 }
