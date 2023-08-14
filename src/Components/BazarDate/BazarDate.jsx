38a119bed8efa8a07e0cad495bdda96e199fdfff
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-14 14:50:18 +0600

    uy

--- src/Components/BazarDate/BazarDate.jsx
+++ src/Components/BazarDate/BazarDate.jsx
@@ -7,12 +7,10 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-// ... (previous imports and code)
 
 export default function BazarDate() {
   const { data } = useSession();
   const [events, setEvents] = useState([]);
-  const [tooltipEvent, setTooltipEvent] = useState(null);
 
   const handleDeleteEvent = (eventToDelete) => {
     if (window.confirm("Are you sure you want to delete this event?")) {
@@ -39,31 +37,26 @@ export default function BazarDate() {
       }
     }
   };
-
-  const handleEventMouseEnter = (arg) => {
-    setTooltipEvent(arg.event);
-  };
-
-  const handleEventMouseLeave = () => {
-    setTooltipEvent(null);
-  };
-
   const renderEventContent = (eventInfo) => {
     return (
       <div
-        className="flex justify-between items-center p-2 bg-gray-100 rounded"
-        onMouseEnter={() => handleEventMouseEnter(eventInfo)}
-        onMouseLeave={handleEventMouseLeave}
+       
+       className="flex justify-between items-center p-2 tooltip tooltip-accent bg-gray-100 rounded"
       >
         <p className="font-semibold text-gray-800">{eventInfo.timeText}</p>
-        {/* set image  */}
-        <Image
+        <div 
+        data-tip={`${data?.user?.name}`}
+        className="tooltip tooltip-accent"
+        
+        >
+          <Image
           className="object-cover rounded-full h-7 w-7"
           src={data?.user?.image || ""}
           alt="avatar"
           width={30}
           height={30}
         />
+        </div>
         <p className="text-gray-800">{eventInfo.event.title}</p>
         <button
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-sm"
@@ -87,8 +80,6 @@ export default function BazarDate() {
         }}
         initialView="dayGridMonth"
         dateClick={handleDateClick}
-        eventMouseEnter={handleEventMouseEnter}
-        eventMouseLeave={handleEventMouseLeave}
         weekends={false}
         nowIndicator={true}
         editable={true}
@@ -97,12 +88,6 @@ export default function BazarDate() {
         eventContent={renderEventContent}
         events={events}
       />
-      {tooltipEvent && (
-        <div className="tooltip">
-          <p>{tooltipEvent.title}</p>
-          <p>{tooltipEvent.extendedProps.description}</p>
-        </div>
-      )}
     </div>
   );
 }
