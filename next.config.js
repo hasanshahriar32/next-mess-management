68bc69c397e2b91c468fd8138c3fc030f6cc9f01
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-24 01:20:01 +0600

    updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com", "i.ibb.co", "*"],
+    domains: [
+      ,
+      ,
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "i.ibb.co",
+      "*",
+      "www.pngkey.com",
+    ],
   },
 };
 
