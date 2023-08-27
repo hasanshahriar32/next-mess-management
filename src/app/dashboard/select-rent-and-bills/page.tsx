159da4ffb4e43e52b362f09700bbfd1e8e046eef
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-27 22:46:54 +0600

    updated bug

--- src/app/dashboard/select-rent-and-bills/page.tsx
+++ src/app/dashboard/select-rent-and-bills/page.tsx
@@ -1,4 +1,4 @@
-import SelectRentAndBills from "@/Components/Dashboard/SelectRentAndBills/SelectRentAndBills";
+// import SelectRentAndBills from "@/Components/Dashboard/SelectRentAndBills/SelectRentAndBills";
 import React from "react";
 
 const page = () => {
