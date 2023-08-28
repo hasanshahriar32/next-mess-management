d66e6446f74804390f39e7d88917df7eb66a7505
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-28 20:34:45 +0600

    updated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,18 +15,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-"use client";
-
-import React from "react";
-import DashboardComponent from "@/Components/Dashboard/DashboardComponent";
-
-const Dashboard = () => {
-  return (
-    <div className="my-16">
-      <DashboardComponent></DashboardComponent>
-    </div>
-  );
-};
-
-export default Dashboard;
