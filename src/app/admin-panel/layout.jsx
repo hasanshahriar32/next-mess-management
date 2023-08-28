63196074e353137a6cb3e5434c90e07c54f30f10
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-28 13:27:50 +0600

    updated

--- src/app/admin-panel/layout.jsx
+++ src/app/admin-panel/layout.jsx
@@ -1,29 +1,53 @@
 "use client";
+import React, { useState, useEffect } from "react"; // Import React, useState, and useEffect
 import Container from "@/Components/ui/Container/container";
-import { ReactNode } from "react";
 import AdminSidebar from "./Sidebar";
 import { useSession } from "next-auth/react";
+import { useGetSingleUserQuery } from "../features/bazar/bazarApi";
 
+const Layout = ({ children }) => {
+  const { status, data } = useSession();
+  
+  const userEmail = data?.user?.email;
+  const [loading, setLoading] = useState(true);
 
+  const { data: session } = useSession();
+  const sessionEmail = session?.user?.email;
+  const { data: singleUser } = useGetSingleUserQuery(userEmail);
 
-const Layout = ( children ) => {
-  const { status, data: session } = useSession();
-  const userRole = session?.user?.role; // Fix here
+  useEffect(() => {
+    if (singleUser !== undefined) {
+      setLoading(false);
+    }
+  }, [singleUser]); // Update loading state when singleUser data changes
 
+  console.log("single user", singleUser);
+  const userRole = singleUser?.user?.role;
+  console.log(data?.user?.email);
   const isAdmin = userRole === "admin" || userRole === "superAdmin";
 
   return (
     <div>
-      {status === "authenticated" && isAdmin ? (
-        <div className="drawer lg:drawer-open">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content">
-            <Container>{children}</Container>
-          </div>
-          <AdminSidebar />
-        </div>
+      {loading ? (
+        <span className="loading loading-bars loading-lg"></span>
       ) : (
-        <h2>Not Admin or SuperUser</h2>
+        <div>
+          {status === "authenticated" && isAdmin ? (
+            <div className="drawer lg:drawer-open">
+              <input
+                id="my-drawer-2"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content">
+                <Container>{children}</Container>
+              </div>
+              <AdminSidebar />
+            </div>
+          ) : (
+            <h2>Not Admin or SuperUser</h2>
+          )}
+        </div>
       )}
     </div>
   );
